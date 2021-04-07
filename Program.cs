bca9a0075d11cec79b8d09c1c24cd373f8a74819
Patil-94 <ankitakhairnar10@.com>
2021-04-07 13:24:23 +0530

    [Ankita] Add:  Employee Attendance

--- Program.cs
+++ Program.cs
@@ -10,6 +10,15 @@ namespace EmpWageComputation
     {
         static void Main(string[] args)
         {
+            int present = 1;
+            Random random = new Random();
+            int employeecheck = random.Next(0, 2);
+            Console.WriteLine("random value" + employeecheck);
+            if (employeecheck == present)
+                Console.WriteLine("Employee is present");
+            else
+                Console.WriteLine("Employee is absent");
+            Console.Read();
         }
     }
 }
