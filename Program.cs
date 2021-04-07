b2ec7e59b6a944a177389e406f6c42aa20543b51
Patil-94 <ankitakhairnar10@.com>
2021-04-07 13:41:40 +0530

    [Ankita] Add:  Daily And Part Time Employee Wage

--- Program.cs
+++ Program.cs
@@ -11,31 +11,37 @@ namespace EmpWageComputation
         static void Main(string[] args)
         {
             //constants
-            int PRESENT = 1;
+            int Full_TIME = 1;
+            int PART_TIME = 2;
             int EMP_RATE_PER_HR = 20;
 
             //local variables
             int empHrs = 0;
-            int empwage = 0;
+            int empWage = 0;
 
             //inbuilt class
             Random random = new Random();
-            int employeeCheck = random.Next(0, 2);
+            int employeeCheck = random.Next(0, 3);
 
             Console.WriteLine("random value " + employeeCheck);
             //selection statements
 
-            if (employeeCheck == PRESENT)
+            if (employeeCheck == Full_TIME)
             {
                 empHrs = 8;
+            }
+            else
+             if (employeeCheck  == PART_TIME)
+            {
+                empHrs = 4;
             }
             else
             {
                 empHrs = 0;
             }
             //computation
-            empwage = EMP_RATE_PER_HR * empHrs;
-            Console.WriteLine("emp wage per day " + empwage);
+            empWage = EMP_RATE_PER_HR * empHrs;
+            Console.WriteLine("Emp wage per day " + empWage);
             Console.Read(); ;
         }
     }
