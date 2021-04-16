714a5db214a3b34fa88f6f3da5c8b0e0edf28fb3
Patil-94 <ankitakhairnar10@.com>
2021-04-16 15:25:51 +0530

    [Ankita] Add:Total Wage Queried By Company

--- CompanyEmpWage.cs
+++ CompanyEmpWage.cs
@@ -32,7 +32,7 @@ namespace EmpWageComputation
 		public string toString() //instance method 
 		{
 
-			return "\nTotal Emp Wage for Company : " + this.company + " is :" + this.totalEmpWage + "\nDaily Wage for Company : " + this.company + " is :" + this.dailyWage + "\nTotal  Emp wage along with daily Wage is " + this.TotalWageAlongWithDailyWage;
+			return "\n Total Emp Wage for Company : " + this.company + " is :" + this.totalEmpWage + "\n Daily Wage for Company : " + this.company + " is :" + this.dailyWage + "\n Total  Emp wage along with daily Wage is " + this.TotalWageAlongWithDailyWage;
 		}
 	}
 }
